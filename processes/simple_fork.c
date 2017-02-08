7fe224d262930f48f9bf7053dc811a5433f7c12c
Goobyalus <jsaina@umd.edu>
2017-02-08 16:35:08 -0500

    initial commit contains a fork example

---
+++ processes/simple_fork.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main() {
+    
+    printf("Program beginning.\n");
+    
+    int status; // Unsure what this argument to wait does!
+    
+    pid_t child_pid = fork();
+    
+    if (-1 == child_pid) {
+        printf("There was an error when forking.\n");
+    } else if (0 == child_pid) {
+        printf("\nI am the child. My pid is %d, and my parent's is %d.\n", getpid(), getppid());
+    } else {
+        printf("\nI am the parent. My pid is %d, and my child's pid is %d. My parent is %d.\n", getpid(), child_pid, getppid());
+        printf("\nFinished waiting for %d. Status: %d.\n\n", wait(&status), status);
+    }
+    
+    return 0;
+}
